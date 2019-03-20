9136178c389174d9eda3be466e0b17d7854484b0
Justas Puodzius <justas.puodzius@gmail.com>
2019-03-20 13:44:12 +0000

    Working duty cycle

--- dutyCycle.cpp
+++ dutyCycle.cpp
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#include <stdlib.h>
 
 #define PINOUT 1
 #define PININ 2
+#define RANGE 1024
 
+int i = 0;
+int u = 0;
+int z = 1;
+float debilizmas = 0;
+float duty =0;
+	
 int main(void)
 {
 	wiringPiSetup();
 	pinMode(PINOUT,PWM_OUTPUT);	
 	pinMode(PININ,INPUT);
+	pwmSetRange(RANGE);
+	pwmWrite(PINOUT,200);
 	
-	pwmWrite(PINOUT,1024);
-	int i = 0;
-	int u = 0;
-	for(int stop = 0; stop<1024; stop++ )
+	while(true)
 	{
-		if(digitalRead(PININ) == 1)
-			i++;
-		else
-			u++;		
+		//system("CLS");
+		i = 0;
+		u = 0;
+		for(int stop = 0; stop<RANGE; stop++ )
+		{
+			if(digitalRead(PININ) == 1)
+				i++;
+			else
+				u++;		
+		}
+		debilizmas +=i;
+		duty = debilizmas/z;
+		duty =(duty/(RANGE)*100);
+		printf("\033[2J\033[1;1H");
+		printf("%3.3f",duty);
+		printf("   %d\n",i);
+		z++;
 	}
-	printf("%d\n", i);
 }
 	
