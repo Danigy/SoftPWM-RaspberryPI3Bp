a5dd6df5af78f4169d333b67d8f03bd8d9a9ca03
Justas Puodzius <justas.puodzius@gmail.com>
2019-03-24 00:27:13 +0000

    pushed dutycycle to thread

--- dutyCycle.cpp
+++ dutyCycle.cpp
@@ -1,49 +1,63 @@
 #include <stdio.h>
 #include <wiringPi.h>
 #include <stdlib.h>
+#include <thread>
+#include <unistd.h>
+#include <iostream>
+#include <ctime>
 
 #define PINOUT 1
 #define PININ 2
 #define RANGE 1024
-#define CLOCK 2
+#define CLOCK 192
 
-int i = 0;
-int u = 0;
-int z = 1;
-float sign = 0;
-float duty =0;
+using namespace std;
 
-void dutyCycle()
-{
-	duty = 0;
-	i = 0;
-	u = 0;
-	for(int stop = 0; stop<RANGE; stop++ )
-	{
-		if(digitalRead(PININ) == 1)
-			i++;	
-	}
-	sign +=i;
-	duty = sign/z;
-	duty =(duty/(RANGE)*100);
-	printf("\033[2J\033[1;1H");
-	printf("%3.3f",duty);
-	printf("   %d\n",i);
-	z++;
+bool risingIndi = false;
+float duty = 0;
+int highCounter = 0;
+
+void risingCounterInterrupt (void)
+{ 
+	risingIndi = true;
 }
-	
+
+void dutyCycle (void)
+{
+	while (true)
+	{	
+		highCounter = 0;		
+		if(risingIndi)
+		{
+			clock_t start = clock();
+			for(int stop = 0; stop<RANGE; stop++ )
+				if(digitalRead(PININ) == 1)					
+					highCounter++;
+			clock_t end = clock();
+					
+			duty = (float)highCounter/(float)RANGE*100;
+			risingIndi = false;
+			
+			//printf("\033[2J\033[1;1H");			
+			printf("%d    ",highCounter);
+			printf("%3.3f    ",duty);
+			cout << (double)(end-start)/CLOCKS_PER_SEC << endl;
+			
+			delayMicroseconds(1000);			
+		}
+		usleep(500);
+	}
+}
+
 int main(void)
 {
 	wiringPiSetup();
-	pinMode(PINOUT,PWM_OUTPUT);	
+	wiringPiISR(PININ, INT_EDGE_RISING, risingCounterInterrupt);	
 	pinMode(PININ,INPUT);
-	pwmSetClock(CLOCK);
-	//pwmSetRange(RANGE);
-	pwmWrite(PINOUT,200);
+	thread cycleLength_t (dutyCycle);
+	cycleLength_t.joinable();
+	int f;
+	cin >> f;
 	
-	while(true)
-	{
-		int edge = wiringPIISR(PININ, INT_EDGE_RISING, dutyCycle);		
-	}
 }
 	
