eb164dbff10c802a72933dbcf6c480a4cbad0016
Justas Puodžius <justas.puodzius@gmail.com>
2019-04-08 11:51:24 +0300

    PWM pakeisti bandymui

--- pwm.cpp
+++ pwm.cpp
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <wiringPi.h>
-#include "EncoderReading.h"
+
+const int pwmpinAf = 1;
+const int pwmpinBf = 23;
+const int pwmpinAb = 24;
+const int pwmpinBb = 26;
 
 
-const int pwmpinA = 1;
-const int pwmpinB = 23;
+void movingMotor(int fwSpeedA, int fwSpeedB, int bwSpeedA, int bwSpeedB) // judinam motor paduodami srove skirtingais pinais.
+{
+		pwmWrite(pwmpinAb,fwSpeedA);
+		pwmWrite(pwmpinBb,fwSpeedB);
+		pwmWrite(pwmpinAf,bwSpeedA);
+		pwmWrite(pwmpinBf,bwSpeedB);
+}
 
 int main(void){
 	wiringPiSetup();
-	pinMode(pwmpinA,PWM_OUTPUT);
-	pinMode(pwmpinB,PWM_OUTPUT);
-	pwmSetMode(PWM_MODE_MS);
-	pwmSetClock(1920);
+	pinMode(pwmpinAf,PWM_OUTPUT);
+	pinMode(pwmpinBf,PWM_OUTPUT);
+	pinMode(pwmpinAb,PWM_OUTPUT);
+	pinMode(pwmpinBb,PWM_OUTPUT);
+	//pwmSetMode(PWM_MODE_MS); - normaliai naudojamas Balanced rezimas, kuris isskirsto mazesniais impulsais, bet 2x dazniau.
+
+	pwmSetClock(1920); // Ciklo daaznis = (19.2/Divisor)/Range
 	pwmSetRange(100);
-	EncoderReading.setInput(2, 3);
-	EncoderReading.encoderSetup();
 	
 	while(true)
 	{
-		pwmWrite(pwmpinA,50);
-		pwmWrite(pwmpinB,50);
+		//------------forward 50%----------
+		movingMotor(50, 50, 0, 0); // movingMotor(fwA, fwB, bwA, bwB)
 		
 		delayMicroseconds(1000000);
-		printf("%d", EncoderReading.getEncoderSteps());
+
+		//------------backward 50%----------
+		movingMotor(0, 0, 50, 50);
 		
-		pwmWrite(pwmpinA, 0);
-		pwmWrite(pwmpinB, 0);
-		
-		delayMicroseconds(1000000);
-		printf("%d", EncoderReading.getEncoderSteps());
-		
-		pwmWrite(pwmpinB,50);
-		pwmWrite(pwmpinA,50);
-		
-		delayMicroseconds(1000000);
-		printf("%d", EncoderReading.getEncoderSteps());
-		
-		pwmWrite(pwmpinB, 0);
-		pwmWrite(pwmpinA, 0);
-		
-		delayMicroseconds(1000000);
-		printf("%d", (EncoderReading.getEncoderSteps()));
+		delayMicroseconds(1000000);	
+
+ 		//--------------break--------------
+        movingMotor(0, 0, 0, 0);
+
 	}
 }
+
+
